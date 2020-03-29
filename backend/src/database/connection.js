61220bb1b966b782260717d016cf5e23a67b87b1
raulmel1o <raulmello@usp.br>
2020-03-29 13:09:53 -0300

    deploy

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile');
 
-const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+const connection = knex(configuration[environment]);
 
 const connection = knex(config);
 
